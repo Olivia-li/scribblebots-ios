48312b26c356c20e7a6ca5b46e3c1d298b8cc607
Olivia Li <oliviali.ouyang@gmail.com>
2022-12-13 13:26:27 -0800

    Initial Commit

---
+++ scribblebots/scribblebotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  scribblebotsApp.swift
+//  scribblebots
+//
+//  Created by Olivia Li on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct scribblebotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
