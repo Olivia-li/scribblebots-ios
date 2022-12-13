1b0719e6b9e8a7dbb85a86e15968881de390ce93
Olivia Li <oliviali.ouyang@gmail.com>
2022-12-13 15:34:50 -0800

    initial commit AR tracking working

---
+++ scribblebots/SkeletonJoint.swift
@@ -0,0 +1,13 @@
+//
+//  SkeletonJoint.swift
+//  scribblebots
+//
+//  Created by Olivia Li on 12/13/22.
+//
+
+import Foundation
+
+struct SkeletonJoint {
+    let name: String
+    var position: SIMD3<Float>
+}
