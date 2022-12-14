2538d4e68058cf5c25d2ee794ccc27314054bc73
Olivia Li <oliviali.ouyang@gmail.com>
2022-12-13 18:36:04 -0800

    Add websocket capiability

--- scribblebots/ARViewContainer.swift
+++ scribblebots/ARViewContainer.swift
@@ -12,35 +12,77 @@ import RealityKit
 
 private var bodySkeleton: BodySkeleton?
 private let bodySkeletonAnchor = AnchorEntity()
+private var webSocket: URLSessionWebSocketTask?
 
 struct ARViewContainer: UIViewRepresentable {
+    
+    
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
         
         arView.setupForBodyTracking()
+        arView.setupForWebSockets()
         arView.scene.addAnchor(bodySkeletonAnchor)
         
         return arView
     }
-    
+        
     
     func updateUIView(_ uiView: ARView, context: Context) {
         
     }
-    
+        
     typealias UIViewType = ARView
-    
-    
-    
     
 }
 
-extension ARView: ARSessionDelegate {
+extension ARView: ARSessionDelegate, URLSessionWebSocketDelegate {
     func setupForBodyTracking() {
         let configuration = ARBodyTrackingConfiguration()
         self.session.run(configuration)
-        
         self.session.delegate = self
+    }
+    
+    func setupForWebSockets() {
+        let urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
+        let url = URL(string: "wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV")
+        webSocket = urlSession.webSocketTask(with: url!)
+        webSocket?.resume()
+    }
+    
+    func ping() {
+        webSocket?.sendPing(pongReceiveHandler: { error in
+            if let error = error {
+                print("Ping error: \(error)")
+            }
+        })
+    }
+    
+    func close() {
+        webSocket?.cancel(with: .goingAway, reason: "Demo ended".data(using: .utf8))
+    }
+    
+    func send() {
+        DispatchQueue.global().asyncAfter(deadline: .now()+1) {
+            self.send()
+            webSocket?.send(.string("Send a new message"), completionHandler: {error in
+                if let error = error {
+                    print("Send error: \(error)")
+                }
+            })
+           
+        }
+    }
+    
+    
+    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        print("Did open socket")
+        ping()
+        send()
+    }
+              
+    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        print("Did close to connection with reason")
     }
     
     public func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
